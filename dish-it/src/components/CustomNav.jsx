ada89613ce17d775d235fb27fae0d1b20b51e626
Christopher Morales <christopher.moraleswebdev@gmail.com>
2023-07-28 15:16:41 -0500

    updated nav logo and started on sign out functionality

--- dish-it/src/components/CustomNav.jsx
+++ dish-it/src/components/CustomNav.jsx
@@ -1,12 +1,16 @@
 import { Link } from 'react-router-dom'
+import { useContext } from 'react'
+import UserContext from '../UserContext'
 
 const CustomNav = () => {
+  const { setUser } = useContext(UserContext)
   return (
     <div className="navbar">
       <div className="nav-links">
             <Link className='nav-link' to='/home'><i className="fa-solid fa-house"></i>Home</Link>
             <Link className='nav-link' to='/create'><i className="fa-solid fa-plus"></i>Create Recipe</Link>
             <Link className='nav-link' to='/favorites'><i className="fa-solid fa-bookmark"></i>Favorites</Link>
+            <Link className='nav-link' to='/'><i className="fa-solid fa-right-from-bracket"></i>Logout</Link>
       </div>
       <div className="nav-logo">
             DISH IT
