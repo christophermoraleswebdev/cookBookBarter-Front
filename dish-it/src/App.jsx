81025c0861a26d7f873dc909fdf19c80b40f0cec
Christopher Morales <christopher.moraleswebdev@gmail.com>
2023-07-31 09:26:21 -0500

    updated logout functionality

--- dish-it/src/App.jsx
+++ dish-it/src/App.jsx
@@ -1,13 +1,23 @@
 import './App.css'
 import CustomNav from './components/CustomNav'
 import Main from '../src/components/Main'
+import Logo from './components/Logo'
+import UserContext from './UserContext'
+import { useState } from 'react'
 
 function App() {
+  const [user, setUser] = useState({
+    email: '', 
+    password: '',
+  })
 
   return (
     <div>
-      <CustomNav />
-      <Main />
+      <UserContext.Provider value={{ user, setUser}}>
+        <CustomNav />
+        <Logo />
+        <Main />
+      </UserContext.Provider>
     </div>
   )
 }
